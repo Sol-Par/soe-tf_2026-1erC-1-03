8bee2a2a09e4e0b33d096e60ccea39ba4d5bc4f1
facusolpar@gmail.com <facusolpar@gmail.com>
2026-06-17 19:57:15 -0300

    Actividad 2

---
+++ soe-tf_02-application/app/src/task_gatekeeper.c
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2026 Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author : Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>
+ */
+
+/********************** inclusions *******************************************/
+/* Project includes */
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Demo includes */
+#include "logger.h"
+#include "dwt.h"
+
+/* Application & Tasks includes */
+#include "board.h"
+#include "app.h"
+#include "display.h"
+
+/********************** macros and definitions *******************************/
+#define G_TASK_GATEKEEPER_CNT_INI	0ul
+
+#define TASK_GATEKEEPER_DEL_ZERO		(pdMS_TO_TICKS(0ul))
+#define TASK_GATEKEEPER_DEL_MAX			(pdMS_TO_TICKS(250ul))
+
+/********************** internal data declaration ****************************/
+
+/********************** internal functions declaration ***********************/
+
+/********************** internal data definition *****************************/
+
+/********************** external data declaration ****************************/
+uint32_t g_task_gatekeeper_cnt;
+
+/********************** external functions definition ************************/
+/* Task thread */
+void task_gatekeeper(void *parameters)
+{
+	/*  Declare & Initialize Task Function variables */
+	g_task_gatekeeper_cnt = G_TASK_GATEKEEPER_CNT_INI;
+
+	display_msg_t mensaje;
+
+	displayInit(DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER);
+
+	/* Print out: Task Initialized */
+	LOGGER_INFO(" ");
+	LOGGER_INFO("  %s is running - Tick [mS] = %lu", pcTaskGetName(NULL), xTaskGetTickCount());
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for (;;)
+	{
+		/* Update Task Counter */
+		g_task_gatekeeper_cnt++;
+
+		// 1. Nos bloqueamos INDEFINIDAMENTE hasta que llegue un mensaje (portMAX_DELAY)
+		// La CPU no pierde tiempo acá: si la cola está vacía, el RTOS corre otras tareas.
+		if (xQueueReceive(h_display_queue, &mensaje, portMAX_DELAY) == pdPASS)
+		{
+			displayCharPositionWrite(mensaje.x, mensaje.y);
+			displayStringWrite(mensaje.p_text);
+
+			vPortFree(mensaje.p_text);
+		}
+
+	}
+}
+
+/********************** end of file ******************************************/
